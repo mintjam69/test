239d27edde19f02e82b80d6f353ba0957bbe9edf
Taras Pererva <taraspererva@gmail.com>
2025-04-03 21:00:15 +0300

    add code

---
+++ script.js
@@ -0,0 +1,30 @@
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting); // "Welcome Mango, your room number is 207!"
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits`;
+
+// const username = "Poly";
+// const message = `Username ${username} is ${username.length} characters long`;
+
+// const courseTopic = "JavaScript essentials";
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length - 1];
+
+// const age = 16;
+// const isAdult = age >= 18;
+
+// const value = "24.5px";
+// numerical = Number.parseFloat(value);
+
+
