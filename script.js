8bfe93e0faf5697d54cd90e651f91e35864907fb
Taras Pererva <taraspererva@gmail.com>
2025-04-11 11:11:57 +0300

    fibo

--- script.js
+++ script.js
@@ -27,4 +27,10 @@
 // const value = "24.5px";
 // numerical = Number.parseFloat(value);
 
+function fibonacci(index) {
+    if (index <= 0) return 0;
+    if (index === 1) return 1;
+    return fibonacci(index - 1) + fibonacci(index - 2);
+}
 
+console.log(fibonacci(0)); // 0
